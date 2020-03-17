ab0ae2a6d67fdc0aa41fb147fd3420dd5880f5c3
Laury Thiebaux <laury.thiebaux1@etu.univ-lorraine.fr>
2020-03-17 08:21:06 +0100

    je sais plus

---
+++ js/principal.js
@@ -0,0 +1,5 @@
+/*
+ * Créer le module principal de l'application qui, lorsque le DOM est chargé, réalise les
+ * différentes initialisations, puis associe le "click" sur le bouton de chargement d'une
+ * galerie à l'action correspondante.
+ */
